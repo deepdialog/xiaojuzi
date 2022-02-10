8ab264851a6b90ccc53c0b6a774ca176ceba288c
段清华DEAN <qhduan@users.noreply.github.com>
2022-02-10 14:24:53 +0800

    reorder scripts (#6)

--- scripts/run.sh
+++ scripts/run.sh
@@ -7,7 +7,7 @@ docker run \
     -e WECHATY_PUPPET_SERVICE_NO_TLS_INSECURE_CLIENT=true \
     -e WECHATY_LOG=verbose \
     -e WECHATY_PUPPET=wechaty-puppet-service \
-    -e WECHATY_PUPPET_SERVICE_TOKEN=puppet_wxwork_97b79b32b77ee639 \
+    -e WECHATY_PUPPET_SERVICE_TOKEN=$WECHATY_PUPPET_SERVICE_TOKEN \
     --network=host \
     --restart=always \
     qhduan/juzibot:0.2
--- webhook/reboot.sh
+++ webhook/reboot.sh
@@ -5,5 +5,5 @@ cd /opt/JuziBot-assist
 git reset --hard
 git pull origin master
 ./scripts/build.sh
-./scripts/update.sh
+WECHATY_PUPPET_SERVICE_TOKEN=xxx ./scripts/update.sh
 (cd fronts && yarn build)
