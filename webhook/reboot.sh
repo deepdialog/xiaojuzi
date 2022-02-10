398e543aef8329f6ee32f7c919d1dca7e95564c8
qhduan <mail@qhduan.com>
2022-02-10 14:44:30 +0800

    update reboot script

--- webhook/reboot.sh
+++ webhook/reboot.sh
@@ -6,4 +6,4 @@ git reset --hard
 git pull origin master
 ./scripts/build.sh
 WECHATY_PUPPET_SERVICE_TOKEN=xxx ./scripts/update.sh
-(cd fronts && yarn build)
+(cd fronts && yarn config set registry https://registry.npm.taobao.org/ && yanr && yarn build)
